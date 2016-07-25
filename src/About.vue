70cbc4e658cf184452b471ddc5e3f435974b9f32
Kushagra Gour <chinchang457@gmail.com>
2016-07-25 17:56:57 +0530

    typo

--- src/About.vue
+++ src/About.vue
@@ -6,7 +6,7 @@
       <p>My cool project is a hackathon starter-kit based on the very awesome <a class="link" href="http://vuejs.org/">Vue.js</a> framework and <a class="link" href="https://firebase.google.com">Firebase</a> as backend.</p>
 
       <p>
-      	To know more about this starter kit, head over to the <a href="https://github.com/chinchang/vuefire-hackathon-starter" class="link">github repo</a>.
+      	To know more about this starter kit, head over to the <a href="https://github.com/chinchang/vuejsfire-hackathon-starter" class="link">github repo</a>.
       </p>
     </section>
   </div>
